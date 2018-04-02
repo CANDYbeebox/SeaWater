5c25af52b3ba85c2a368664f93791d67d48e6f50
SURE <707849011@qq.com>
2018-04-02 21:43:37 +0800

    海水淡化项目部分

---
+++ src/main/java/com/exception/NullException.java
@@ -0,0 +1,12 @@
+package com.exception;
+
+/**
+ * Created by try on 2018/3/28.
+ */
+public class NullException extends RuntimeException {
+
+    public NullException(String message) {
+        super(message);
+    }
+
+}
